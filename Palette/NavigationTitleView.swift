3df1ac791487f4daf64314646cb696ef10ace305
Alex Mathers <Malecks@Alexs-MacBook-Pro.local>
2017-06-01 13:38:20 -0400

    Add custom NavigationTitleView

---
+++ Palette/NavigationTitleView.swift
@@ -0,0 +1,65 @@
+//
+//  NavigationTitleView.swift
+//  Palette
+//
+//  Created by Alex Mathers on 2017-06-01.
+//  Copyright © 2017 Malecks. All rights reserved.
+//
+
+import UIKit
+
+final class NavigationTitleView: UIView {
+
+    @IBOutlet private weak var containerView: UIView!
+    @IBOutlet private weak var leftIcon: UIImageView!
+    @IBOutlet private weak var centerIcon: UIImageView!
+    @IBOutlet private weak var rightIcon: UIImageView!
+    @IBOutlet private weak var horizontalConstraint: NSLayoutConstraint!
+    
+    private lazy var icons: [UIImageView] = {
+        return [self.leftIcon, self.centerIcon, self.rightIcon]
+    }()
+    
+    lazy var iconOffset: CGFloat = {
+        return (self.containerView.frame.width / 2.0) - (self.leftIcon.frame.width / 2.0)
+    }()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        for icon in icons {
+            icon.image = icon.image?.withRenderingMode(.alwaysTemplate)
+            icon.tintColor = .lightGray
+        }
+    }
+
+    func scroll(direction: UIPageViewControllerNavigationDirection, to index: Int) {
+        guard index < icons.count else { return }
+        
+        horizontalConstraint.constant += direction == .reverse ? self.iconOffset : -self.iconOffset
+        
+        UIView.animate(withDuration: 0.25, delay: 0.0, options: .curveEaseOut, animations: {
+            self.highlightIcon(at: index)
+            self.setNeedsLayout()
+            self.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    func highlightIcon(at index: Int) {
+        guard index < icons.count else { return }
+        
+        for (i, icon) in icons.enumerated() {
+            icon.tintColor = i == index ? .black : .lightGray
+        }
+    }
+    
+    class func instanceFromNib() -> UIView? {
+        let nib = UINib(nibName: "NavigationTitleView", bundle: nil)
+            .instantiate(withOwner: nil, options: nil)
+        for view in nib {
+            if let view = view as? NavigationTitleView {
+                return view
+            }
+        }
+        return nil
+    }
+}
--- Palette/PageViewController.swift
+++ Palette/PageViewController.swift
@@ -11,13 +11,18 @@ import UIKit
 final class PageViewController: UIPageViewController {
     
     fileprivate var viewControllersArray: [UIViewController]!
+    fileprivate lazy var titleView: NavigationTitleView? = {
+        return NavigationTitleView.instanceFromNib() as? NavigationTitleView
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         dataSource = self
-        self.setupViews()
-        self.setupNavigationBar()
-        self.setViewControllers([self.viewControllersArray[1]], direction: .forward, animated: true, completion: nil)
+        delegate = self
+        setupViews()
+        setupNavigationBar()
+        setViewControllers([viewControllersArray[1]], direction: .forward, animated: true, completion: nil)
     }
     
     private func setupViews() {
@@ -28,56 +33,103 @@ final class PageViewController: UIPageViewController {
         let palettesViewController = storyBoard.instantiateViewController(withIdentifier: "PaletteView") as! PalettesViewController
         
         cameraViewController.didSavePalette = {
-            self.setViewControllers([palettesViewController], direction: .forward, animated: true, completion: { complete in
-                if complete {
+            self.setViewControllers([palettesViewController], direction: .forward, animated: true, completion: { finished in
+                if finished {
                     palettesViewController.scrollToTop()
+                    self.titleView?.scroll(direction: .forward, to: 1)
                 }
             })
         }
         
         inspirationViewController.didSavePalette = {
-            self.setViewControllers([palettesViewController], direction: .reverse, animated: true, completion: { complete in
-                if complete {
-                    palettesViewController.scrollToTop()
-                }
+            self.setViewControllers([palettesViewController],
+                                    direction: .reverse,
+                                    animated: true,
+                                    completion: { complete in
+                                        if complete {
+                                            palettesViewController.scrollToTop()
+                                            self.titleView?.scroll(direction: .reverse, to: 1)
+                                        }
             })
         }
         
         palettesViewController.showCameraView = {
-            self.setViewControllers([cameraViewController], direction: .reverse, animated: true, completion: nil)
+            self.setViewControllers([cameraViewController],
+                                    direction: .reverse,
+                                    animated: true,
+                                    completion: { complete in
+                                        if complete {
+                                            palettesViewController.scrollToTop()
+                                            self.titleView?.scroll(direction: .reverse, to: 0)
+                                        }
+            })
         }
         
         palettesViewController.showInspirationView = {
-            self.setViewControllers([inspirationViewController], direction: .forward, animated: true, completion: nil)
+            self.setViewControllers([inspirationViewController],
+                                    direction: .forward,
+                                    animated: true,
+                                    completion: { complete in
+                                        if complete {
+                                            palettesViewController.scrollToTop()
+                                            self.titleView?.scroll(direction: .forward, to: 2)
+                                        }
+            })
         }
         
-        self.viewControllersArray = [cameraViewController, palettesViewController, inspirationViewController]
+        viewControllersArray = [cameraViewController, palettesViewController, inspirationViewController]
     }
 
     private func setupNavigationBar() {
         guard let navBar = self.navigationController?.navigationBar else { return }
-        navBar.tintColor = .black
         navBar.layer.shadowOffset = CGSize(width: 0, height: 5)
         navBar.layer.shadowRadius = 0
         navBar.layer.shadowOpacity = 0.1
+
+        navigationItem.titleView = titleView
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if let vc = viewControllers?.first,
+            let index = viewControllersArray.index(of: vc) {
+            titleView?.highlightIcon(at: index)
+        }
     }
 }
 
 extension PageViewController: UIPageViewControllerDataSource {
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        let currentIndex =  self.viewControllersArray.index(of: viewController)!-1
+        let currentIndex =  viewControllersArray.index(of: viewController)!-1
         if currentIndex < 0 {
             return nil
         }
-        return self.viewControllersArray[currentIndex]
+        return viewControllersArray[currentIndex]
         
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        let currentIndex =  self.viewControllersArray.index(of: viewController)!+1
-        if currentIndex >= self.viewControllersArray.count {
+        let currentIndex =  viewControllersArray.index(of: viewController)!+1
+        if currentIndex >= viewControllersArray.count {
             return nil
         }
-        return self.viewControllersArray[currentIndex]
+        return viewControllersArray[currentIndex]
+    }
+}
+
+extension PageViewController: UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
+
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        guard let titleView = self.titleView,
+            let previousVC = previousViewControllers.first,
+            let previousIndex = viewControllersArray.index(of: previousVC),
+            let currentVC = viewControllers?.first,
+            let currentIndex = viewControllersArray.index(of: currentVC),
+            currentIndex != previousIndex else { return }
+        
+        titleView.scroll(direction: currentIndex > previousIndex ? .forward : .reverse, to: currentIndex)
     }
 }
